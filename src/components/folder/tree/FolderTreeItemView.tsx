022aae4b5be048cbbe0ab063be1b3171c77193db
Spencer Hoffa <s_hoffa@hotmail.com>
2025-11-02 09:25:11 -0700

    added move modal

--- src/components/folder/tree/FolderTreeItemView.tsx
+++ src/components/folder/tree/FolderTreeItemView.tsx
@@ -5,9 +5,12 @@ import { FolderItem } from "../FolderItem";
 import styles from "./FolderTree.module.css";
 import { useDrag, useDrop } from "react-dnd";
 import { DragTypes } from "../dnd/DragTypes";
+import { Button, Modal } from "react-bootstrap";
 
 export default function FolderTreeItemView({data, selectFolderFunc, selectedFolderState}:{data:FolderItem, selectFolderFunc:(selectedFolderId:number)=>void, selectedFolderState:number}){
-    let [isExpanded, setIsExpanded] = useState(false);    
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [folderToMove, setFolderToMove] = useState<FolderItem|undefined>(undefined);
+    const [folderToMoveTo, setFolderToMoveTo] = useState<FolderItem|undefined>(undefined);
 
     function toggleDropdownEvent(event: MouseEvent<HTMLElement>){
         setIsExpanded(!isExpanded);
@@ -29,7 +32,9 @@ export default function FolderTreeItemView({data, selectFolderFunc, selectedFold
             if (monitor.didDrop()) {
                 return;
             }
-            alert(item.id + " dropped into " + data.id);
+            setFolderToMove(item);
+            setFolderToMoveTo(data);
+            setShow(true);
         },
         canDrop: (item:FolderItem, monitor) => {
             return !item.isDescendant(data)
@@ -42,35 +47,61 @@ export default function FolderTreeItemView({data, selectFolderFunc, selectedFold
         })
     }));
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleMoveFolder = async () => {
+      setShow(false);
+
+      // TODO: move folder code goes here
+    };
+
     return (
-        <div ref={el=>{drag(el)}} 
-                className={`${data.children.length > 0 
-                                ? styles.FolderContinuityLine_NoChildPadding 
-                                : `${styles.FolderContinuityLine} 
-                                    ${styles.FolderContinuityLine_ChildPadding}`} 
-                                    ${isDragging && styles.FolderTree_Dragging}`}>
-            {data.children.length > 0 && 
-                <i className={`${styles.ExpanderBase} bi ${isExpanded ? "bi-chevron-down" : "bi-chevron-right"}`} 
-                    onClick={toggleDropdownEvent} />
-            }
-            <a ref={el=>{drop(el)}} 
-                className={`${styles.FolderName} 
-                            ${selectedFolderState === data.id && styles.Foldername_Selected} 
-                            ${isOver && (canDrop ? styles.FolderTree_Droppable : styles.FolderTree_NotDroppable)}`} 
-                onClick={() => selectFolderFunc(data.id)}>
-                <span ref={el=>{preview(el)}}>{data.name}</span>
-            </a>
-            <div className={isExpanded ? styles.FolderContinuityLine : "d-none"}>
-                <ul className={styles.ChildList}>
-                    {data.children.map((child: FolderItem) => (
-                        <li key={child.id}>
-                            <FolderTreeItemView data={child} 
-                                                selectFolderFunc={selectFolderFunc} 
-                                                selectedFolderState={selectedFolderState}/>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
+      <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Move Folder</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <span>Are you sure you want to move the '{folderToMove?.name}' folder into the '{folderToMoveTo?.name}' folder?</span>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+              Close
+          </Button>
+          <Button variant="primary" onClick={handleMoveFolder}>
+              Move Folder
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div ref={el=>{drag(el)}} 
+              className={`${data.children.length > 0 
+                              ? styles.FolderContinuityLine_NoChildPadding 
+                              : `${styles.FolderContinuityLine} 
+                                  ${styles.FolderContinuityLine_ChildPadding}`} 
+                                  ${isDragging && styles.FolderTree_Dragging}`}>
+          {data.children.length > 0 && 
+              <i className={`${styles.ExpanderBase} bi ${isExpanded ? "bi-chevron-down" : "bi-chevron-right"}`} 
+                  onClick={toggleDropdownEvent} />
+          }
+          <a ref={el=>{drop(el)}} 
+              className={`${styles.FolderName} 
+                          ${selectedFolderState === data.id && styles.Foldername_Selected} 
+                          ${isOver && (canDrop ? styles.FolderTree_Droppable : styles.FolderTree_NotDroppable)}`} 
+              onClick={() => selectFolderFunc(data.id)}>
+              <span ref={el=>{preview(el)}}>{data.name}</span>
+          </a>
+          <div className={isExpanded ? styles.FolderContinuityLine : "d-none"}>
+              <ul className={styles.ChildList}>
+                  {data.children.map((child: FolderItem) => (
+                      <li key={child.id}>
+                          <FolderTreeItemView data={child} 
+                                              selectFolderFunc={selectFolderFunc} 
+                                              selectedFolderState={selectedFolderState}/>
+                      </li>
+                  ))}
+              </ul>
+          </div>
+      </div>
+      </>
     );
 }
